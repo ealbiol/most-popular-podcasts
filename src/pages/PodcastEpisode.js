c0d284ae3e9f323db5c8c08fbddc785cc8998f50
ealbiol <eduard.albiol@gmail.com>
2023-04-13 18:50:59 +0200

    Player HTML added.

--- src/pages/PodcastEpisode.js
+++ src/pages/PodcastEpisode.js
@@ -26,18 +26,24 @@ export default function PodcastEpisode() {
                     <CardPodcaster name={name} artist={artist} description={description} imageUrl={imageUrl} />
                 </Box>
                 <Box>
-                    <Card sx={{maxWidth:"700px"}}>
+                    <Card sx={{ maxWidth: "700px" }}>
                         <CardContent>
                             <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
                                 {selectedEpisode.trackName.substring(selectedEpisode.trackName.indexOf('|') + 1).trim().replace(/^"(.*)"$/, '$1')}
                             </Typography>
-                            <Typography sx={{marginBottom:"18px"}} variant="body2">
+                            <Typography sx={{ marginBottom: "18px" }} variant="body2">
                                 {selectedEpisode.shortDescription}
                             </Typography>
                             <Divider />
 
-                            <Typography sx={{marginTop: "18px", marginBottom:"18px"}} variant="body2">
-                                Reserved Space
+                            <Typography sx={{ marginTop: "18px" }} variant="body2">
+                                <audio style={{width:"100%"}} controls>
+                                    <source src={selectedEpisode.episodeUrl} type="audio/mpeg" />
+                                    Your browser does not support the audio element.
+                                </audio>
+
+
+
                             </Typography>
                         </CardContent>
                     </Card>
